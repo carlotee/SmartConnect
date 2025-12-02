0d6d47f76e64bb3536edc9df1659346a45826beb
Carlos Vega <carlosvega3031@gmail.com>
2025-12-02 12:18:57 -0300

    se modifica settins

--- SmartConnect/settings.py
+++ SmartConnect/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'gestion_dispositivos',
+    'control_acceso',
+    'usuarios',
 ]
 
 MIDDLEWARE = [
