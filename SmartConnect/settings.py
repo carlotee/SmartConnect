29c82a5b7b269bf84bbaf9b05928ac9c2774787d
TrashLoaded <alexiseba15@gmail.com>
2025-12-08 22:06:04 -0300

    luco

--- SmartConnect/settings.py
+++ SmartConnect/settings.py
@@ -80,6 +80,12 @@ MIDDLEWARE = [
 
 CORS_ALLOW_ALL_ORIGINS = True
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8080", 
+    "https://tu.dominio.frontend",
+    "http://98.95.183.73"
+]
+
 ROOT_URLCONF = 'SmartConnect.urls'
 
 TEMPLATES = [
