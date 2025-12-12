cee57e57976f35b66b6cb81cc08dcdd342480cd8
Laboratorio Computación N° 9 <claseslab9@inacap.cl>
2025-12-12 12:16:01 -0300

    luco

--- utils/exceptions.py
+++ utils/exceptions.py
@@ -1,9 +1,14 @@
 from rest_framework.views import exception_handler
+from rest_framework.response import Response
 
 def custom_exception_handler(exc, context):
     response = exception_handler(exc, context)
-    
-    if response is not None and 'detail' in response.data:
-        response.data = {'error': str(response.data['detail'])}
+
+    if response is not None:
+        if response.status_code == 401:
+            view = context.get('view', None)
+            if view and getattr(view, 'permission_classes', None):
+                if any(cls.__name__ == 'AllowAny' for cls in view.permission_classes):
+            response.data = {"error": "Acceso denegado"}
     
     return response
