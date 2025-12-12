1e3c8ba058c0612d63d271160512e85b4a63b8dc
Laboratorio Computación N° 9 <claseslab9@inacap.cl>
2025-12-12 13:44:29 -0300

    luco

--- utils/exceptions.py
+++ utils/exceptions.py
@@ -1,11 +1,21 @@
 from rest_framework.views import exception_handler
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.exceptions import AuthenticationFailed, NotAuthenticated
+from rest_framework.exceptions import AuthenticationFailed, NotAuthenticated, PermissionDenied
 
 def custom_exception_handler(exc, context):
+
     if isinstance(exc, (AuthenticationFailed, NotAuthenticated)):
-        return Response({"error": "Sin Autenticación"}, status=status.HTTP_401_UNAUTHORIZED)
+        return Response(
+            {"Error": "Sin Autenticación"},
+            status=status.HTTP_401_UNAUTHORIZED
+        )
+
+    if isinstance(exc, PermissionDenied):
+        return Response(
+            {"Error": "Sin Permisos"},
+            status=status.HTTP_403_FORBIDDEN
+        )
 
     response = exception_handler(exc, context)
     return response
