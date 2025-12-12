bb99ed72b6bb694ee07dadf1958e7a62f13c66ee
Laboratorio Computación N° 9 <claseslab9@inacap.cl>
2025-12-12 12:37:45 -0300

    luco

--- utils/exceptions.py
+++ utils/exceptions.py
@@ -1,14 +1,12 @@
 from rest_framework.views import exception_handler
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.exceptions import AuthenticationFailed, NotAuthenticated
+from rest_framework.exceptions import NotAuthenticated, AuthenticationFailed
 
 def custom_exception_handler(exc, context):
     response = exception_handler(exc, context)
 
-    if response is not None:
-        if isinstance(exc, (AuthenticationFailed, NotAuthenticated)):
-            response.data = {"error": "Sin Autenticación"}
-            response.status_code = status.HTTP_401_UNAUTHORIZED
+    if isinstance(exc, (NotAuthenticated, AuthenticationFailed)):
+        return Response({"error": "Sin Autenticación"}, status=status.HTTP_401_UNAUTHORIZED)
 
     return response
