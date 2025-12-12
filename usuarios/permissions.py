5af049f064bc542143fbecd8fcccb26e47fa0055
Laboratorio Computación N° 9 <claseslab9@inacap.cl>
2025-12-12 13:33:57 -0300

    luco

--- usuarios/permissions.py
+++ usuarios/permissions.py
@@ -1,12 +1,12 @@
 from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 class IsAdminOrReadOnly(BasePermission):
-
     def has_permission(self, request, view):
+
+        if request.method in SAFE_METHODS:
+            return request.user.is_authenticated
+
         if not request.user.is_authenticated:
             return False
 
-        if request.method in SAFE_METHODS:
-            return True
-
-        return request.user.rol == "admin"
+        return request.user.rol == 'admin'
