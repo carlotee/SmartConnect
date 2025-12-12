f4c823fea65b7f8aa620dc6bd1e3e0e0c51ba9a0
Laboratorio Computación N° 9 <claseslab9@inacap.cl>
2025-12-12 13:36:09 -0300

    luco

--- usuarios/views.py
+++ usuarios/views.py
@@ -3,13 +3,11 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import AllowAny
-
 from .serializers import UsuarioSerializer, DepartamentoSerializer
 from .models import UsuarioPersonalizado
 from gestion_dispositivos.models import Departamento
-
 from .permissions import IsAdminOrReadOnly
-
+from rest_framework.permissions import IsAuthenticated
 
 class UsuarioViewSet(viewsets.ModelViewSet):
     queryset = UsuarioPersonalizado.objects.all()
