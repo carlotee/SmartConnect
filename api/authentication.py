1ebcf934df3965a982947836591905aafc582f55
Laboratorio Computación N° 9 <claseslab9@inacap.cl>
2025-12-12 11:48:13 -0300

    luco

--- api/authentication.py
+++ api/authentication.py
@@ -3,8 +3,7 @@ from rest_framework.exceptions import AuthenticationFailed
 
 class CustomJWTAuthentication(JWTAuthentication):
     def authenticate(self, request):
-        try:
-            return super().authenticate(request)
-        except AuthenticationFailed:
-            raise AuthenticationFailed(detail="No autorizado")
-
+        user_auth_tuple = super().authenticate(request)
+        if user_auth_tuple is None:
+            raise AuthenticationFailed({'error': 'No autorizado'})
+        return user_auth_tuple
--- utils/exceptions.py
+++ utils/exceptions.py
@@ -3,8 +3,8 @@ from rest_framework.views import exception_handler
 def custom_exception_handler(exc, context):
     response = exception_handler(exc, context)
 
-    if response is not None and response.status_code == 401:
-        response.data.clear()
-        response.data['Error'] = str(exc.detail)  
-
+    if response is not None:
+        if 'error' in response.data:
+            response.data = {'error': response.data['detail']}
+    
     return response
