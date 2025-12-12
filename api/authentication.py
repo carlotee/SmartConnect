8936ee24703cba841e580a4fd9035dc700ca4bb5
Laboratorio Computación N° 9 <claseslab9@inacap.cl>
2025-12-12 12:44:40 -0300

    luco

--- api/authentication.py
+++ api/authentication.py
@@ -1,17 +1,18 @@
 from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.exceptions import NotAuthenticated
 
 class CustomJWTAuthentication(JWTAuthentication):
     def authenticate(self, request):
         header = self.get_header(request)
         if header is None:
-            return None
+            raise NotAuthenticated("Sin Autenticación")
 
         raw_token = self.get_raw_token(header)
         if raw_token is None:
-            return None
+            raise NotAuthenticated("Sin Autenticación")
 
         try:
             validated_token = self.get_validated_token(raw_token)
             return self.get_user(validated_token), validated_token
         except Exception:
-            return None
+            raise NotAuthenticated("Sin Autenticación")
--- utils/exceptions.py
+++ utils/exceptions.py
@@ -4,10 +4,8 @@ from rest_framework import status
 from rest_framework.exceptions import AuthenticationFailed, NotAuthenticated
 
 def custom_exception_handler(exc, context):
-    # Interceptamos errores de autenticación
     if isinstance(exc, (AuthenticationFailed, NotAuthenticated)):
         return Response({"error": "Sin Autenticación"}, status=status.HTTP_401_UNAUTHORIZED)
 
-    # Para otros errores, usamos el manejador por defecto
     response = exception_handler(exc, context)
     return response
