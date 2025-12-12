dbc826c6d4bdeee25e96bec3cc4e77daf79a564f
Laboratorio Computación N° 9 <claseslab9@inacap.cl>
2025-12-12 11:30:00 -0300

    luco

--- SmartConnect/settings.py
+++ SmartConnect/settings.py
@@ -53,7 +53,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'path.to.CustomJWTAuthentication',
+    'api.authentication.CustomJWTAuthentication',
     ),
     'EXCEPTION_HANDLER': 'utils.exceptions.custom_exception_handler'
 }
