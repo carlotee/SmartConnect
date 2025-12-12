76d7963fcc78a58d26fb247efd91289b829dba74
Laboratorio Computación N° 9 <claseslab9@inacap.cl>
2025-12-12 13:42:23 -0300

    luco

--- usuarios/permissions.py
+++ usuarios/permissions.py
@@ -1,11 +1,15 @@
 from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 class IsAdminOrReadOnly(BasePermission):
+    message = "Sin Permisos"  # mensaje para errores 403
+
     def has_permission(self, request, view):
 
+        # Si es GET/HEAD/OPTIONS → solo necesita estar autenticado
         if request.method in SAFE_METHODS:
             return request.user.is_authenticated
 
+        # Para POST / PUT / PATCH / DELETE → debe ser admin
         if not request.user.is_authenticated:
             return False
 
