6969897d506832be34237021600df105f0cadfea
TrashLoaded <alexiseba15@gmail.com>
2025-12-14 20:54:16 -0300

    luco

--- api/authentication.py
+++ api/authentication.py
@@ -1,5 +1,6 @@
 from rest_framework_simplejwt.authentication import JWTAuthentication
-from rest_framework.exceptions import AuthenticationFailed
+from rest_framework.exceptions import NotAuthenticated
+from rest_framework_simplejwt.exceptions import InvalidToken, TokenError
 
 class CustomJWTAuthentication(JWTAuthentication):
     def authenticate(self, request):
@@ -10,11 +11,12 @@ class CustomJWTAuthentication(JWTAuthentication):
 
         raw_token = self.get_raw_token(header)
         if raw_token is None:
-            return None
+            raise NotAuthenticated("Sin Autenticación")
 
         try:
             validated_token = self.get_validated_token(raw_token)
             user = self.get_user(validated_token)
             return (user, validated_token)
-        except Exception:
-            raise AuthenticationFailed("Token inválido o expirado")
+
+        except (InvalidToken, TokenError):
+            raise NotAuthenticated("Sin Autenticación")
--- usuarios/permissions.py
+++ usuarios/permissions.py
@@ -6,9 +6,9 @@ class IsAdminOrReadOnly(BasePermission):
     def has_permission(self, request, view):
 
         if request.method in SAFE_METHODS:
-            return request.user.is_authenticated
+            return request.user and request.user.is_authenticated
 
-        if not request.user.is_authenticated:
+        if not request.user or not request.user.is_authenticated:
             return False
 
         return request.user.rol == 'admin'
